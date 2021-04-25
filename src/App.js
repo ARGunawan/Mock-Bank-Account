5bb56ee3012bf47cd900a6dd624e1689efa94ae1
jamyooes <james.yoo715@gmail.com>
2021-04-25 17:58:36 -0400

    Simply added a credit.js

--- src/App.js
+++ src/App.js
@@ -1,44 +1,48 @@
 // src/App.js
 
-import React, {Component} from 'react';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
-import Home from './components/Home';
-import UserProfile from './components/UserProfile';
-import LogIn from './components/LogIn';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from "./components/Home";
+import UserProfile from "./components/UserProfile";
+import LogIn from "./components/LogIn";
 
 class App extends Component {
-
   constructor() {
     super();
 
     this.state = {
       accountBalance: 14568.27,
       currentUser: {
-        userName: 'joe_shmo',
-        memberSince: '07/23/96',
-      }
-    }
+        userName: "joe_shmo",
+        memberSince: "07/23/96",
+      },
+    };
   }
 
   render() {
-
-    const HomeComponent = () => (<Home accountBalance={this.state.accountBalance}/>);
-    const UserProfileComponent = () => (
-        <UserProfile userName={this.state.currentUser.userName} memberSince={this.state.currentUser.memberSince}  />
+    const HomeComponent = () => (
+      <Home accountBalance={this.state.accountBalance} />
     );
-    const LogInComponent = () => (<LogIn user={this.state.currentUser} mockLogIn={this.mockLogIn} />);
+    const UserProfileComponent = () => (
+      <UserProfile
+        userName={this.state.currentUser.userName}
+        memberSince={this.state.currentUser.memberSince}
+      />
+    );
+    const LogInComponent = () => (
+      <LogIn user={this.state.currentUser} mockLogIn={this.mockLogIn} />
+    );
 
     return (
-        <Router>
-          <div>
-            <Route exact path="/" render={HomeComponent}/>
-            <Route exact path="/userProfile" render={UserProfileComponent}/>
-            <Route exact path="/login" render={LogInComponent}/>
-          </div>
-        </Router>
+      <Router>
+        <div>
+          <Route exact path="/" render={HomeComponent} />
+          <Route exact path="/userProfile" render={UserProfileComponent} />
+          <Route exact path="/login" render={LogInComponent} />
+        </div>
+      </Router>
     );
   }
-
 }
 
 export default App;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,19 +1,22 @@
 // src/components/Home.js
-import React, {Component} from 'react';
-import AccountBalance from './AccountBalance';
-import {Link} from 'react-router-dom';
+import React, { Component } from "react";
+import AccountBalance from "./AccountBalance";
+import { Link } from "react-router-dom";
 
 class Home extends Component {
   render() {
     return (
-        <div>
-          <img src="https://letstalkpayments.com/wp-content/uploads/2016/04/Bank.png" alt="bank"/>
-          <h1>Bank of React</h1>
+      <div>
+        <img
+          src="https://letstalkpayments.com/wp-content/uploads/2016/04/Bank.png"
+          alt="bank"
+        />
+        <h1>Bank of React</h1>
 
-          <Link to="/userProfile">User Profile</Link>
+        <Link to="/userProfile">User Profile</Link>
 
-          <AccountBalance accountBalance={this.props.accountBalance}/>
-        </div>
+        <AccountBalance accountBalance={this.props.accountBalance} />
+      </div>
     );
   }
 }
