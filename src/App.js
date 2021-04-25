4c83fde51ea1fc34199a80f869a4fd7ce8052b87
jamyooes <james.yoo715@gmail.com>
2021-04-25 18:55:49 -0400

    small changes

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@
 
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import axios from 'axios';
+import axios from "axios";
 import Home from "./components/Home";
 import UserProfile from "./components/UserProfile";
 import LogIn from "./components/LogIn";
 import Credits from "./components/Credits";
-import Debits from "/components/Debits";
+import Debits from "./components/Debits";
 
 class App extends Component {
   constructor() {
@@ -25,30 +25,39 @@ class App extends Component {
     this.updateDebitBalance = this.updateDebitBalance.bind(this);
     this.updateDebitInfo = this.updateDebitInfo.bind(this);
   }
-  
-  componentDidMount = () =>{
-    //getting the debits when app is mounted    
+
+  componentDidMount = () => {
+    //getting the debits when app is mounted
     axios
-      .get('https://moj-api.herokuapp.com/debits') //get the data
+      .get("https://moj-api.herokuapp.com/debits") //get the data
       .then((response) => {
         const data = response.data;
         //console.log(data);
         let holder = []; // hold the data for now
-        for( let a = 0; a< data.length; a++) //go through all the data
-        {
+        for (
+          let a = 0;
+          a < data.length;
+          a++ //go through all the data
+        ) {
           holder = [data[a].description, data[a].amount, data[a].date]; //load the data
-          this.setState({debitInfo:[...this.state.debitInfo, holder], debitBalance: this.state.debitBalance + data[a].amount})
+          this.setState({
+            debitInfo: [...this.state.debitInfo, holder],
+            debitBalance: this.state.debitBalance + data[a].amount,
+          });
         }
       })
       .catch((err) => console.log(err));
-  }
+  };
 
   //update functions
-  updateDebitBalance(event){
-    this.setState({accountBalance:this.state.accountBalance - event, debitBalance:this.state.debitBalance + event})
+  updateDebitBalance(event) {
+    this.setState({
+      accountBalance: this.state.accountBalance - event,
+      debitBalance: this.state.debitBalance + event,
+    });
   }
-  updateDebitInfo(event){
-    this.setState({debitInfo: [event, ...this.state.debitInfo]})
+  updateDebitInfo(event) {
+    this.setState({ debitInfo: [event, ...this.state.debitInfo] });
   }
 
   render() {
@@ -65,7 +74,14 @@ class App extends Component {
       <LogIn user={this.state.currentUser} mockLogIn={this.mockLogIn} />
     );
     const CreditComponent = () => <Credits />;
-    const DebitsComponent = () =>(<Debits updateDebitBalance={this.updateDebitBalance} updateDebitInfo={this.updateDebitInfo} debitInfo={this.state.debitInfo} debitBalance={this.state.debitBalance}/>);
+    const DebitsComponent = () => (
+      <Debits
+        updateDebitBalance={this.updateDebitBalance}
+        updateDebitInfo={this.updateDebitInfo}
+        debitInfo={this.state.debitInfo}
+        debitBalance={this.state.debitBalance}
+      />
+    );
 
     return (
       <Router>
@@ -74,7 +90,7 @@ class App extends Component {
           <Route exact path="/userProfile" render={UserProfileComponent} />
           <Route exact path="/login" render={LogInComponent} />
           <Route exact path="/credits" render={CreditComponent} />
-          <Route exact path="/debits" render={DebitsComponent}/>
+          <Route exact path="/debits" render={DebitsComponent} />
         </div>
       </Router>
     );
